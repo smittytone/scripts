7af899927fdc3075b2904faf3770586db132b34f
smittytone <smittytone@icloud.com>
2019-04-02 08:17:08 +0100

    Add in-process messaging

--- toserver.sh
+++ toserver.sh
@@ -2,7 +2,7 @@
 # NOTE You may need to change the above line to /bin/bash
 
 # Backup to Server Script
-# Version 1.1.2
+# Version 1.1.3
 
 clear
 echo "Backup to Server"
@@ -22,24 +22,29 @@ if ! [ -f $bookmark ]; then
 fi
 
 while IFS= read -r line; do
-    if ! [ -d .mntpoint ]; then
-        mkdir .mntpoint
+    if ! [ -d mntpoint ]; then
+        echo "Making mntpoint..."
+        mkdir mntpoint
     fi
 
-    if ! [ -d .mntpoint/home ]; then
-        mkdir .mntpoint/home
+    if ! [ -d mntpoint/home ]; then
+        echo "Making mntpoint/home..."
+        mkdir mntpoint/home
     fi
 
-    if ! [ -d .mntpoint/music ]; then
-        mkdir .mntpoint/music
+    if ! [ -d mntpoint/music ]; then
+        echo "Making mntpoint/music..."
+        mkdir mntpoint/music
     fi
 
-    mount -t smbfs "//$line@192.168.0.3/music" .mntpoint/music
+    echo "Mounting mntpoint/home..."
+    mount -t smbfs "//$line@192.168.0.3/music" mntpoint/music
     if [ $? -eq 0 ]; then
         musicMounted=1
     fi
 
-    mount -t smbfs "//$line@192.168.0.3/home"  .mntpoint/home
+    echo "Mounting mntpoint/home..."
+    mount -t smbfs "//$line@192.168.0.3/home"  mntpoint/home
     if [ $? -eq 0 ]; then
         homeMounted=1
     fi
@@ -52,30 +57,30 @@ if [ $count -eq 0 ]; then
     exit 1
 fi
 
-if [[ -d .mntpoint/home && $homeMounted -eq 1 ]]; then
-    echo "Backing-up Comics and Books"
-    rsync -avz ~/Documents/Comics .mntpoint/home --exclude ".*"
-    rsync -avz ~/OneDrive/eBooks .mntpoint/home --exclude ".*"
+if [[ -d mntpoint/home && $homeMounted -eq 1 ]]; then
+    echo "Backing-up Comics and Books..."
+    rsync -avz ~/Documents/Comics mntpoint/home --exclude ".*"
+    rsync -avz ~/OneDrive/eBooks mntpoint/home --exclude ".*"
 else
     echo "The server’s HOME partition is not mounted -- backup cannot continue"
 fi
 
-if [[ -d .mntpoint/music && $musicMounted -eq 1 ]]; then
-    echo "Backing-up Music"
-    rsync -avz ~/Music/Alternative .mntpoint/music --exclude ".DS_Store"
-    rsync -avz ~/Music/Classical .mntpoint/music --exclude ".DS_Store"
-    rsync -avz ~/Music/Comedy .mntpoint/music --exclude ".DS_Store"
-    rsync -avz ~/Music/'Doctor Who' .mntpoint/music --exclude ".DS_Store"
-    rsync -avz ~/Music/Electronic .mntpoint/music --exclude ".DS_Store"
-    rsync -avz ~/Music/Folk .mntpoint/music --exclude ".DS_Store"
-    rsync -avz ~/Music/Metal .mntpoint/music --exclude ".DS_Store"
-    rsync -avz ~/Music/Pop .mntpoint/music --exclude ".DS_Store"
-    rsync -avz ~/Music/Rock .mntpoint/music --exclude ".DS_Store"
-    rsync -avz ~/Music/SFX .mntpoint/music --exclude ".DS_Store"
-    rsync -avz ~/Music/Soundtracks .mntpoint/music --exclude ".DS_Store"
-    rsync -avz ~/Music/'Spoken Word' .mntpoint/music --exclude ".DS_Store"
-    #rsync -avz ~/Music/Ringtones .mntpoint/music --exclude ".DS_Store"
-    #rsync -avz ~/Music/Singles .mntpoint/music --exclude ".DS_Store"
+if [[ -d mntpoint/music && $musicMounted -eq 1 ]]; then
+    echo "Backing-up Music..."
+    rsync -avz ~/Music/Alternative mntpoint/music --exclude ".DS_Store"
+    rsync -avz ~/Music/Classical mntpoint/music --exclude ".DS_Store"
+    rsync -avz ~/Music/Comedy mntpoint/music --exclude ".DS_Store"
+    rsync -avz ~/Music/'Doctor Who' mntpoint/music --exclude ".DS_Store"
+    rsync -avz ~/Music/Electronic mntpoint/music --exclude ".DS_Store"
+    rsync -avz ~/Music/Folk mntpoint/music --exclude ".DS_Store"
+    rsync -avz ~/Music/Metal mntpoint/music --exclude ".DS_Store"
+    rsync -avz ~/Music/Pop mntpoint/music --exclude ".DS_Store"
+    rsync -avz ~/Music/Rock mntpoint/music --exclude ".DS_Store"
+    rsync -avz ~/Music/SFX mntpoint/music --exclude ".DS_Store"
+    rsync -avz ~/Music/Soundtracks mntpoint/music --exclude ".DS_Store"
+    rsync -avz ~/Music/'Spoken Word' mntpoint/music --exclude ".DS_Store"
+    #rsync -avz ~/Music/Ringtones mntpoint/music --exclude ".DS_Store"
+    #rsync -avz ~/Music/Singles mntpoint/music --exclude ".DS_Store"
 else
     echo "The server's MUSIC directory is not mounted -- backup cannot continue"
 fi
@@ -85,25 +90,30 @@ echo
 
 # Unmount the shares; keep the operation success value for each
 if [ $musicMounted -eq 1 ]; then
-    umount .mntpoint/music
+    echo "Dismounting mntpoint/home..."
+    umount mntpoint/music
     success1=$?
 fi
 
 if [ $homeMounted -eq 1 ]; then
-    umount .mntpoint/home
+    echo "Dismounting mntpoint/home..."
+    umount mntpoint/home
     success2=$?
 fi
 
 # Make sure the unmount operations succeeded, warning if not
 if [[ $success1 -eq 0 && $success2 -eq 0 ]]; then
     # Remove the share mount points if both unmounts were successful
-    rm -r .mntpoint
+    echo "Removing mntpoint..."
+    rm -r mntpoint
 else
     if [ $success1 -ne 0 ]; then
-        echo ~+"/.mntpoint/music failed to unmount -- please unmount it manually and remove the mointpoint"
+        echo ~+"/mntpoint/music failed to unmount -- please unmount it manually and remove the mointpoint"
     fi
 
     if [ $success2 -ne 0 ]; then
-        echo ~+"/.mntpoint/home failed to unmount -- please unmount it manually and remove the mointpoint"
+        echo ~+"/mntpoint/home failed to unmount -- please unmount it manually and remove the mointpoint"
     fi
 fi
+
+echo Done
