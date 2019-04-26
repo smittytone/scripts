1fec98e8af4b81acb12d6a49bb8d44766a14ecbf
smittytone <smittytone@icloud.com>
2019-04-26 12:44:40 +0100

    Update zinstall.sh

--- zinstall.sh
+++ zinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Pi Zero Installation Script 1.0.0
+# Pi Zero Installation Script 1.0.1
 
 # Switch to home directory
-cd /home/pi
+cd "$HOME"
 
 # Remove stock folders
 echo "Removing home sub-directories..."
@@ -27,7 +27,8 @@ mkdir Python
 
 # Update .bashrc
 echo -e "\nConfiguring command line..."
-echo "export PS1='$PWD > '" >> .bashrc
+echo "export PS1='$(pwd) > '" >> .bashrc
+echo "GIT=~/Documents/GitHub" >> .bashrc
 echo "alias la='ls -lahF --color=auto'" > .bash_aliases
 echo "alias ls='ls -lhF --color=auto'" >> .bash_aliases
 echo "alias rs='sudo shutdown -r now'" >> .bash_aliases
@@ -36,37 +37,44 @@ echo "alias python=python3" >> .bash_aliases
 echo "alias update='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y'" >> .bash_aliases
 export PATH=$PATH:/usr/local/bin
 
-# Wifi setup
-
-read -p "Enter your WiFi SSID: "
-if [ -z "$REPLY" ]; then
-    echo "Not a vaild SSID -- cancelling..."
-    exit 1
-fi
-ssid=$REPLY
-
-read -s -p "Enter your WiFi password (just hit [ENTER] for no password): "
-if [ -z "$REPLY" ]; then
-    read -n 1 -s -p "You have entered no password -- is that correct?"
-    exit 1
-fi
-psk=$REPLY
-
-sudo echo "network={ ssid=$ssid psk=$psk }" >> /etc/wpa_supplicant/wpa_supplicant.conf
-
 # Applications
 echo -e "\nAdding utilities..."
 sudo apt-get install -y screen nginx ruby scrot
 sudo gem install mdless
+sudo pip3 install pylint
 
 # Node
 version="10.13.0"
 echo -e "\nInstalling Node..."
-mdkir tmp
-cd tmp
+mkdir tmp
+cd tmp || exit 1
 wget "https://nodejs.org/dist/v$version/node-v$version-linux-armv6l.tar.gz"
 tar -xzf "node-v$version-linux-armv6l.tar.gz"
 cd "node-v$version-linux-armv6l"
 sudo cp -R * /usr/local/
-cd ~
+
+# Git
+echo -e "\nCloning key repos..."
+cd ~/Documents/GitHub || exit 1
+git clone https://github.com/smittytone/dotfiles.git
+git clone https://github.com/smittytone/scripts.git
+
+# Setup configs
+cp dotfiles/nanorc "$HOME"/.nanorc
+cp dotfiles/pylintrc "$HOME"/.pylintrc
+cp dotfiles/gitignore_global "$HOME"/.gitignore_global
+git config --global core.excludesfile "$HOME"/.gitignore_global
+
+echo -e "\nCleaning up..."
+# Remove the script
+cd "$HOME"
+rm zinstall.sh
 rm -r tmp
+
+read -n 1 -s -p "Press any key to reboot (or [C] to cancel)" key
+key=${key^^*}
+if [ "$key" != "C" ]; then
+    sudo shutdown -r now
+else
+    echo
+fi
