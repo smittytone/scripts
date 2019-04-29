ba2b9d280f3a77bb6af53049eaf7a78a85a9ccd3
smittytone <smittytone@icloud.com>
2019-04-29 09:06:00 +0100

    Modify path quoting; update to 1.0.2

--- upconf.sh
+++ upconf.sh
@@ -3,7 +3,7 @@
 #      but I use brew-installed bash under macOS
 
 # Update local user config files
-# Version 1.0.1
+# Version 1.0.2
 
 source=~/documents/github/dotfiles
 target="$HOME/Library"
@@ -27,42 +27,41 @@ if [[ "$choice" != "F" && "$choice" != "P" ]]; then
 fi
 
 echo "Updating primary config files... "
-cp -v "$source"/nanorc "$HOME"/.nanorc
-cp -v "$source"/bash_profile "$HOME"/.bash_profile
+cp -v "$source/nanorc" "$HOME/.nanorc"
+cp -v "$source/bash_profile" "$HOME/.bash_profile"
 
 if ! [ -e "$HOME"/.config ]; then
     echo "Adding ~/.config... "
-    mkdir "$HOME"/.config
+    mkdir "$HOME/.config"
 fi
 
 if ! [ -e "$HOME"/.config/gitup ]; then
     echo "Adding ~/.config/gitup... "
-    mkdir "$HOME"/.config/gitup
+    mkdir "$HOME/.config/gitup"
 fi
 
-cp -v "$source"/bookmarks "$HOME"/.config/gitup/bookmarks;
-cp -v "$source"/vs_settings.json "$target/Application Support/Code/User/settings.json"
+cp -v "$source/bookmarks" "$HOME/.config/gitup/bookmarks"
+cp -v "$source/vs_settings.json" "$target/Application Support/Code/User/settings.json"
 
 if [ "$choice" = "F" ]; then
     echo "Updating additional config files... "
     # These are items that won't be overwritten
 
-    cp -nvR "$source"/Services/'Copy File Path'.workflow "$target"/Services/'Copy File Path'.workflow
-    cp -nvR "$source"/LaunchAgents "$target"/LaunchAgents
-    cp -nvR "$source"/Quicklook "$target"/Quicklook
-
+    cp -nvR "$source/Services/Copy File Path.workflow" "$target/Services/Copy File Path.workflow"
+    cp -nvR "$source/LaunchAgents" "$target/LaunchAgents"
+    cp -nvR "$source/Quicklook" "$target/Quicklook"
 
     echo "Adding ~/.config/git... "
     if ! [ -e "$HOME"/.config/git ]; then
         mkdir ~/.config/git
     fi
 
-    if cp -nv "$source"/gitignore_global "$HOME"/.config/git/gitignore_global; then
+    if cp -nv "$source/gitignore_global" "$HOME/.config/git/gitignore_global"; then
         # Add a reference to the file to git (assumes git installed)
-        git config --global core.excludesfile "$HOME"/.config/git/gitignore_global
+        git config --global core.excludesfile "$HOME/.config/git/gitignore_global"
     fi
 
-    cp -nv "$source"/HomebrewMe.terminal "$HOME"/Desktop/HomebrewMe.terminal
+    cp -nv "$source/HomebrewMe.terminal" "$HOME/Desktop/HomebrewMe.terminal"
     echo "Terminal settings file 'HomebrewMe' copied to desktop. To use it, open Terminal > Preferences > Profiles and import"
 fi
 
