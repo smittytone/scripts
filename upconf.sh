e1415d3869edae8f9072dd052a3f5b547dda4ec4
smittytone <smittytone@icloud.com>
2019-05-01 11:18:23 +0100

    Fix CRs; always update global gitignore

--- upconf.sh
+++ upconf.sh
@@ -3,7 +3,7 @@
 #      but I use brew-installed bash under macOS
 
 # Update local user config files
-# Version 1.0.2
+# Version 1.0.3
 
 source=~/documents/github/dotfiles
 target="$HOME/Library"
@@ -15,52 +15,55 @@ fi
 
 read -n 1 -s -p "Full [F] or partial [P] update? " choice
 if [ -z "$choice" ]; then
-    echo -e "/nCancelling..."
+    echo -e "\nCancelling..."
     exit 0
 fi
 
 choice=${choice^^*}
 
 if [[ "$choice" != "F" && "$choice" != "P" ]]; then
-    echo -e "/nInvalid option selected ($choice) -- cancelling... "
+    echo -e "\nInvalid option selected: '$choice' -- cancelling... "
     exit 0
 fi
 
-echo "Updating primary config files... "
+# The following are items that are likely to change often
+echo -e "\nUpdating primary config files... "
+
+# nano rc file
 cp -v "$source/nanorc" "$HOME/.nanorc"
+
+# bash profile
 cp -v "$source/bash_profile" "$HOME/.bash_profile"
 
-if ! [ -e "$HOME"/.config ]; then
-    echo "Adding ~/.config... "
-    mkdir "$HOME/.config"
-fi
-
-if ! [ -e "$HOME"/.config/gitup ]; then
+# gitup config
+if ! [ -e "$HOME/.config/gitup" ]; then
     echo "Adding ~/.config/gitup... "
-    mkdir "$HOME/.config/gitup"
+    mkdir -p "$HOME/.config/gitup"
+fi
+cp -v "$source/bookmarks" "$HOME/.config/gitup/bookmarks"
+
+if ! [ -e "$HOME/.config/git" ]; then
+    echo "Adding ~/.config/git... "
+    mkdir -p "$HOME/.config/git"
 fi
 
-cp -v "$source/bookmarks" "$HOME/.config/gitup/bookmarks"
+# git global exclude file
+# Added it partial install in 1.0.3
+if cp -v "$source/gitignore_global" "$HOME/.config/git/gitignore_global"; then
+    # Add a reference to the file to git (assumes git installed)
+    git config --global core.excludesfile "$HOME/.config/git/gitignore_global"
+fi
+
+# vscode settings
 cp -v "$source/vs_settings.json" "$target/Application Support/Code/User/settings.json"
 
+# The following are items that won't be overwritten. They are very, very unlikely
+# to be changed once installed in the first place
 if [ "$choice" = "F" ]; then
     echo "Updating additional config files... "
-    # These are items that won't be overwritten
-
     cp -nvR "$source/Services/Copy File Path.workflow" "$target/Services/Copy File Path.workflow"
     cp -nvR "$source/LaunchAgents" "$target/LaunchAgents"
     cp -nvR "$source/Quicklook" "$target/Quicklook"
-
-    echo "Adding ~/.config/git... "
-    if ! [ -e "$HOME"/.config/git ]; then
-        mkdir ~/.config/git
-    fi
-
-    if cp -nv "$source/gitignore_global" "$HOME/.config/git/gitignore_global"; then
-        # Add a reference to the file to git (assumes git installed)
-        git config --global core.excludesfile "$HOME/.config/git/gitignore_global"
-    fi
-
     cp -nv "$source/HomebrewMe.terminal" "$HOME/Desktop/HomebrewMe.terminal"
     echo "Terminal settings file 'HomebrewMe' copied to desktop. To use it, open Terminal > Preferences > Profiles and import"
 fi
