d889880e5df91c7d36ac6b52fc404089e93f01e7
smittytone <smittytone@icloud.com>
2019-06-03 15:25:18 +0100

    Make script more scripty

--- todisk.sh
+++ todisk.sh
@@ -2,45 +2,36 @@
 # NOTE You may need to change the above line to /bin/bash
 
 # Backup to Disk Script
-# Version 1.0.2
+# Version 1.1.0
+
+target_vol=2TB-APFS
+target_path="/Volumes/$target_vol"
+
+d_sources=("/Documents/Comics" "/OneDrive/eBooks")
+m_sources=("/Music/Alternative" "/Music/Classical" "/Music/Comedy" "/Music/Doctor Who"
+           "/Music/Electronic" "/Music/Folk" "/Music/Pop" "/Music/Metal" "/Music/Rock"
+           "/Music/SFX" "/Music/Singles" "/Music/Soundtracks" "/Music/Spoken Word")
 
 clear
 echo "Backup to Disk"
-read -n 1 -s -p "Connect MEDIA2 then press [ENTER] when it has mounted"
-echo 
+read -n 1 -s -p "Connect '$target_vol' then press [ENTER] when it has mounted"
+echo
 
-if [ -d /Volumes/MEDIA2 ]; then
-    echo "Disk MEDIA2 mounted."
-    echo "Syncing Comics"
-    rsync -avz ~/Documents/Comics /Volumes/MEDIA2 --exclude ".DS_Store"
-    echo "Syncing eBooks"
-    rsync -avz ~/OneDrive/eBooks /Volumes/MEDIA2 --exclude ".DS_Store"
-    echo "Syncing Music:Alternative"
-    rsync -avz ~/Music/Alternative /Volumes/MEDIA2/Music --exclude ".DS_Store"
-    echo "Syncing Music:Classical"
-    rsync -avz ~/Music/Classical /Volumes/MEDIA2/Music --exclude ".DS_Store"
-    echo "Syncing Music:Comedy"
-    rsync -avz ~/Music/Comedy /Volumes/MEDIA2/Music --exclude ".DS_Store"
-    echo "Syncing Music:Doctor Who"
-    rsync -avz ~/Music/'Doctor Who' /Volumes/MEDIA2/Music --exclude ".DS_Store"
-    echo "Syncing Music:Electronic"
-    rsync -avz ~/Music/Electronic /Volumes/MEDIA2/Music --exclude ".DS_Store"
-    echo "Syncing Music:Folk"
-    rsync -avz ~/Music/Folk /Volumes/MEDIA2/Music --exclude ".DS_Store"
-    echo "Syncing Music:Pop"
-    rsync -avz ~/Music/Pop /Volumes/MEDIA2/Music --exclude ".DS_Store"
-    echo "Syncing Music:Metal"
-    rsync -avz ~/Music/Metal /Volumes/MEDIA2/Music --exclude ".DS_Store"
-    echo "Syncing Music:Rock"
-    rsync -avz ~/Music/Rock /Volumes/MEDIA2/Music --exclude ".DS_Store"
-    echo "Syncing Music:SFX"
-    rsync -avz ~/Music/SFX /Volumes/MEDIA2/Music --exclude ".DS_Store"
-    echo "Syncing Music:Singles"
-    rsync -avz ~/Music/Singles /Volumes/MEDIA2/Music --exclude ".DS_Store"
-    echo "Syncing Music:Soundtracks"
-    rsync -avz ~/Music/Soundtracks /Volumes/MEDIA2/Music --exclude ".DS_Store"
-    echo "Syncing Music:Spoken Word"
-    rsync -avz ~/Music/'Spoken Word' /Volumes/MEDIA2/Music --exclude ".DS_Store"
+if [ -d "$target_path" ]; then
+    echo "Disk '$target_vol' mounted."
+    # Sync document sources
+    for source in "${d_sources[@]}"; do
+        name="${source##*/}"
+        echo "Syncing $name"
+        rsync -avz "$HOME/$source" "$target_path" --exclude ".DS_Store"
+    done
+
+    # Sync music sources
+    for source in "${m_sources[@]}"; do
+        name="${source##*/}"
+        echo "Syncing $name music"
+        rsync -avz "$HOME/$source" "$target_path/Music" --exclude ".DS_Store"
+    done
 else
-    echo "Disk MEDIA2 is not mounted."
+    echo "Disk '$target_vol' is not mounted."
 fi
