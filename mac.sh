f93197a1f6777f7a04a44a06c3c1b0c64661ac00
smittytone <smittytone@icloud.com>
2019-06-03 18:01:36 +0100

    Add many more settings

--- mac.sh
+++ mac.sh
@@ -1,14 +1,118 @@
-#!/usr/local/bin/bash
-# NOTE You may need to change the above line to /bin/bash
+#!/bin/bash
 
 # Mac install script
-# Version 1.0.1
+# Version 1.0.3
 
 # Do intro
 clear
-echo "macOS Install Script 1.0.1"
+echo "macOS Install Script 1.0.3"
 
-# Get server access
+# Update macOS
+softwareupdate --install --all
+
+# Apply preferred Energy Saver settings
+sudo pmset -a lessbright 0
+sudo pmset -a disksleep 10
+sudo pmset -b displaysleep 15
+sudo pmset -b sleep 15
+sudo pmset -b powernap 0
+sudo pmset -c displaysleep 60
+sudo pmset -c sleep 60
+sudo pmset -c powernap 1
+
+# Ask for and set the machine's machine name
+read -p "Enter your preferred hostname " hostname
+echo -e "\nSetting machine name to $hostname"
+if [ -n "$hostname" ]; then
+    sudo scutil --set HostName "$hostname"
+    sudo scutil --set LocalHostName "$hostname"
+    sudo scutil --set ComputerName "$hostname"
+    sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$hostname"
+    dscacheutil -flushcache
+fi
+
+# Set dark mode
+osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
+
+# Clean up Home folder items
+echo -n "Hiding Home folder items: "
+chflags hidden "$HOME/Movies"
+echo -n "Movies, "
+chflags hidden "$HOME/Public"
+echo "Public"
+echo "Showing the Library folder..."
+chflags nohidden "$HOME/Library"
+
+# Set up git and clone key repos
+echo "Preparing Git..."
+target="$HOME/Documents/GitHub"
+if ! [ -d "$target" ]; then
+    mdkir "$target"
+fi
+
+cd "$target" || exit 1
+git clone https://github.com/smittytone/scripts.git
+git clone https://github.com/smittytone/dotfiles.git
+
+# Run the udpate app config scripts
+"$target/scripts/upconf.sh"
+
+# Run the various mac config scripts
+cd "$target/scripts/config"
+for task in *; do
+    . "$task"
+done
+
+# Restart Finder and Dock to effect changes
+killall Finder Dock
+
+# Install applications... brew first
+echo "Installing Brew... "
+if /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; then
+    echo "Installing Brew-sourced Utilities... "
+    apps=("bash" "nano" "coreutils" "gitup" "jq" "ncurses" "readline" "shellcheck" "gitup" "libdvdcss" "node" "python3")
+    for app in "${apps[@]}"; do
+        brew install "$app"
+    done
+
+    echo "Installing Applications... "
+    apps=("handbrake" "vlc" "skype" "firefox" "omnidisksweeper" "google-chrome" "zoomus")
+    for app in "${apps[@]}"; do
+        brew cask install "$app"
+    done
+fi
+
+echo "Installing Cocoapods (requires authorizaton)... "
+sudo gem install cocoapods
+
+echo "Installing Pylint... "
+pip3 install pylint
+
+read -n 1 -s -p "Press [ENTER] to open websites for other app downloads, or [S] to skip " key
+echo
+key=${key^^*}
+if [ "$key" != "S" ]; then
+    open http://www.dropbox.com
+    open http://www.barebones.com
+    open https://desktop.github.com
+    open http://www.rogueamoeba.com/piezo
+    open https://www.bresink.com/osx/0TinkerTool/download.php
+    open http://www.audacityteam.org/download/mac/
+    #open http://www.skype.com
+    #open http://handbrake.fr
+    #open http://www.mozilla.org
+    #open http://www.skype.com
+    #open http://www.videolan.org
+fi
+
+read -n 1 -s -p "Press [ENTER] to open the App Store, or [S] to skip " key
+echo
+key=${key^^*}
+if [ "$key" != "S" ]; then
+    open "/Applications/App Store.app"
+fi
+
+# Get server access for remaining items
 read -p "Please enter your server username: "
 echo
 if [ -z "$REPLY" ]; then
@@ -38,131 +142,8 @@ if mount -v -t smbfs "//$credo@192.168.0.3/homes/macsource" mntpoint; then
     cp -nvR mntpoint/fonts "$HOME/$target/Fonts"
     echo "Copying xroar ROMs from server..."
     cp -nvR mntpoint/xroar "$HOME/$target/xroar"
-
-    # Copy these dylibs then make the required aliases
-    cp -va mntpoint/ffmpeg "$HOME/Library/ffmpeg-mac-2.2.2"
-    for file in "$HOME"/Library/ffmpeg-mac-2.2.2; do
-        if [ -f "$file" ]; then
-            extension=${file##*.}
-            extension=${extension^^*}
-
-            if [ "$extension" != "TXT" ]; then
-                linkname="${file//[0-9]/}"
-                linkname="${linkname//../.}"
-                ln -s "$file" "$linkname"
-            fi
-        fi
-    done
 else
     echo "Unable to copy files from server (return code: $?)"
-fi
-
-echo -n "Hiding Home folder items: "
-chflags hidden "$HOME"/Movies
-echo -n "Movies, "
-chflags hidden "$HOME"/Public
-echo "Public"
-
-echo "Showing the Home Library folder..."
-chflags nohidden "$HOME"/Library
-
-echo "Preparing Git..."
-target="$HOME/Documents/GitHub"
-if ! [ -d "$target" ]; then
-    mdkir "$target"
-fi
-
-cd "$target" || exit 1
-git clone https://github.com/smittytone/scripts.git
-git clone https://github.com/smittytone/dotfiles.git
-
-source="$target/dotfiles"
-echo "Updating primary config files... "
-cp -v "$source/nanorc" "$HOME/.nanorc"
-cp -v "$source/bash_profile" "$HOME/.bash_profile"
-
-if ! [ -e "$HOME"/.config ]; then
-    echo "Adding ~/.config... "
-    mkdir "$HOME"/.config
-fi
-
-if ! [ -e "$HOME"/.config/gitup ]; then
-    echo "Adding ~/.config/gitup... "
-    mkdir "$HOME"/.config/gitup
-fi
-
-cp -v "$source/bookmarks" "$HOME/.config/gitup/bookmarks"
-cp -v "$source/vs_settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
-cp -vR "$source/Services/Copy File Path.workflow" "$HOME/Library/Services/Copy File Path.workflow"
-cp -vR "$source/LaunchAgents" "$HOME/Library/LaunchAgents"
-cp -vR "$source/Quicklook" "$HOME/Library/Quicklook"
-cp -vR "$source/ffmpeg" "$target/ffmpeg"
-
-echo "Adding ~/.config/git... "
-if ! [ -e "$HOME"/.config/git ]; then
-    mkdir ~/.config/git
-fi
-
-if cp -nv "$source"/gitignore_global "$HOME"/.config/git/gitignore_global; then
-    # Add a reference to the file to git (assumes git installed)
-    git config --global core.excludesfile "$HOME"/.config/git/gitignore_global
-fi
-
-cp -nv "$source/HomebrewMe.terminal" "$HOME/Desktop/HomebrewMe.terminal"
-echo "Terminal settings file 'HomebrewMe' copied to desktop. To use it, open Terminal > Preferences > Profiles and import "
-
-cp -nv "$source/pixelmator_shapes.pxs" "$HOME/Desktop/pixelmator_shapes.pxs"
-echo "Pixelmater shapes file 'pixelmator_shapes.pxs' copied to desktop. To use it, open Pixelmator > File > Import..."
-
-echo "Installing Brew... "
-if /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; then
-    echo "Brew installed -- installing Brew-sourced Applications..."
-    brew install bash
-    brew install coreutils
-    brew install gitup
-    brew install jq
-    brew install ncurses
-    brew install readline
-    brew install shellcheck
-    brew install gitup
-    brew install libdvdcss
-fi
-
-echo "Installing Cocoapods (requires authorizaton)... "
-sudo gem install cocoapods
-
-echo "Installing Pylint... "
-pip3 install pylint
-
-echo "Installing apps... "
-brew cask install handbrake
-brew cask install vlc
-brew cask install skype
-brew cask install firefox
-brew cask install omnidisksweeper
-
-read -n 1 -s -p "Press [ENTER] to open websites for app downloads, or [S] to skip " key
-echo
-key=${key^^*}
-if [ "$key" != "S" ]; then
-    open http://www.dropbox.com
-    open http://www.barebones.com
-    open https://desktop.github.com
-    #open http://www.mozilla.org
-    open http://www.rogueamoeba.com/piezo
-    #open http://www.skype.com
-    #open http://handbrake.fr
-    open https://www.bresink.com/osx/0TinkerTool/download.php
-    open http://www.audacityteam.org/download/mac/
-    #open http://www.skype.com
-    #open http://www.videolan.org
-fi
-
-read -n 1 -s -p "Press [ENTER] to open the App Store, or [S] to skip " key
-echo
-key=${key^^*}
-if [ "$key" != "S" ]; then
-    open "/Applications/App Store.app"
 fi
 
 read -n 1 -s -p "Connect drive '2TB-APFS' and press [ENTER] to copy music, or [S] to skip " key
@@ -176,10 +157,11 @@ read -n 1 -s -p "Press [ENTER] to finish "
 echo
 
 echo "Cleaning up... "
-# Unmount the share; keep the operation success value for each
-# Make sure the unmount operations succeeded, warning if not
+brew cleanup
+
+# Unmount the share
 if umount mntpoint; then
-    # Remove the share mount points if both unmounts were successful
+    # Remove the share mount points if the unmount was successful
     rm mntpoint
 else
     echo "$(pwd)/mntpoint failed to unmount -- please unmount it manually and remove the mointpoint "
