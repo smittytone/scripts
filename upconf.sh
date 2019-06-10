9370b52103cddbeadd89cdb3971a870da820a611
smittytone <smittytone@icloud.com>
2019-06-10 17:38:25 +0100

    Update upconf.sh

--- upconf.sh
+++ upconf.sh
@@ -2,19 +2,19 @@
 # NOTE You may need to change the above line to /bin/bash
 #      but I use brew-installed bash under macOS
 
-# Update local user config files
+# Update local user config files (eg. between multiple machines)
 # Version 1.0.5
 
 source="$HOME/documents/github/dotfiles"
 target="$HOME/Library"
 
 if ! [ -e "$source" ]; then
-    echo "Please clone the repo \'dotfiles\' before proceeding-- exiting "
+    echo "Please clone the repo \'dotfiles\' before proceeding -- exiting "
     exit 1
 fi
 
 # Process any arguments
-choice="N"
+choice="ASK"
 for arg in "$@"; do
     if [[ $arg = "-f" || $arg = "--full" ]]; then
         choice="F"
@@ -23,7 +23,7 @@ for arg in "$@"; do
     fi
 done
 
-if [ "$choice" == "N" ]; then
+if [ "$choice" = "ASK" ]; then
     read -n 1 -s -p "Full [F] or partial [P] update? " choice
     if [ -z "$choice" ]; then
         echo -e "\nCancelling..."
@@ -46,6 +46,9 @@ cp -v "$source/nanorc" "$HOME/.nanorc"
 # bash profile
 cp -v "$source/bash_profile" "$HOME/.bash_profile"
 
+# vscode settings
+cp -v "$source/vs_settings.json" "$target/Application Support/Code/User/settings.json"
+
 # gitup config
 if ! [ -e "$HOME/.config/gitup" ]; then
     echo "Adding ~/.config/gitup... "
@@ -59,14 +62,11 @@ if ! [ -e "$HOME/.config/git" ]; then
 fi
 
 # git global exclude file
-# Added it partial install in 1.0.3
+# Added it to partial install in 1.0.3
 if cp -v "$source/gitignore_global" "$HOME/.config/git/gitignore_global"; then
     # Add a reference to the file to git (assumes git installed)
     git config --global core.excludesfile "$HOME/.config/git/gitignore_global"
 fi
-
-# vscode settings
-cp -v "$source/vs_settings.json" "$target/Application Support/Code/User/settings.json"
 
 # The following are items that won't be overwritten. They are very, very unlikely
 # to be changed once installed in the first place
@@ -75,13 +75,14 @@ if [ "$choice" = "F" ]; then
     cp -nvR "$source/Services/Copy File Path.workflow" "$target/Services/Copy File Path.workflow"
     cp -nvR "$source/LaunchAgents" "$target/LaunchAgents"
     cp -nvR "$source/Quicklook" "$target/Quicklook"
-    cp -nv "$source/HomebrewMe.terminal" "$HOME/Desktop/HomebrewMe.terminal"
-    echo "Terminal settings file 'HomebrewMe' copied to desktop. To use it, open Terminal > Preferences > Profiles and import"
     cp -nvR "$source/bbedit_squirrel.plist" "$target/Application Support/BBEdit/Language Modules/Squirrel.plist"
-    cp -nv "$source/pixelmator_shapes.pxs" "$HOME/Desktop/pixelmator_shapes.pxs"
-    echo "Pixelmater shapes file 'pixelmator_shapes.pxs' copied to desktop. To use it, open Pixelmator > File > Import..."
     cp -nvR "$source/ffmpeg" "$target/ffmpeg"
 
+    cp -nv "$source/HomebrewMe.terminal" "$HOME/Desktop/HomebrewMe.terminal"
+    echo "Terminal settings file 'HomebrewMe' copied to desktop. To use it, open Terminal > Preferences > Profiles and import"
+
+    cp -nv "$source/pixelmator_shapes.pxs" "$HOME/Desktop/pixelmator_shapes.pxs"
+    echo "Pixelmater shapes file 'pixelmator_shapes.pxs' copied to desktop. To use it, open Pixelmator > File > Import..."
 fi
 
 echo Done
