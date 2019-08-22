ef1b677ae16b2343aeb0a5b409ccce3c16934d6c
smittytone <smittytone@icloud.com>
2019-08-22 16:20:17 +0100

    Update gitfix.sh

--- gitfix.sh
+++ gitfix.sh
@@ -2,11 +2,12 @@
 # NOTE You may need to change the above line to /bin/bash
 
 echo "GitFix"
-read -n 1 p "You should be in your project directory. Press [G] to continue, or [C] to cancel " key
+read -n 1 p "You should be in your project directory. Press [G] to continue, or any other key to cancel " key
 key=${key^^*}
 
-if [ $key = "G" ]; then
+if [ "$key" = "G" ]; then
     git rm -r --cached .
     git add .
     git commit -m ".gitignore is now working"
+    echo "Git Fixed"
 fi
