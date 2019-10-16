5d35985600c4ae0521d2501496fe654052bf367d
smittytone <smittytone@icloud.com>
2019-10-16 16:18:03 +0100

    Fix 'read' line bug
    (missing - on the p switch)

--- gitfix.sh
+++ gitfix.sh
@@ -2,12 +2,13 @@
 # NOTE You may need to change the above line to /bin/bash
 
 echo "GitFix"
-read -n 1 p "You should be in your project directory. Press [G] to continue, or any other key to cancel " key
+read -n 1 -s -p "You should be in your project directory. Press [G] to continue, or any other key to cancel " key
 key=${key^^*}
 
 if [ "$key" = "G" ]; then
     git rm -r --cached .
     git add .
     git commit -m "[Git Fixed] .gitignore is now working"
+
     echo "Git Fixed"
 fi
