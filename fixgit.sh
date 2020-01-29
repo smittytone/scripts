9a274b44c3508e7c82c7f93ec2a64c6fef9fd27a
smittytone <smittytone@icloud.com>
2020-01-29 13:41:21 +0000

    Rename gitfix -> fixgit

--- fixgit.sh
+++ fixgit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "GitFix"
+echo "FixGit"
 read -n 1 -s -p "You should be in your project directory. Press [G] to continue, or any other key to cancel " key
 key=${key^^*}
 
@@ -8,6 +8,5 @@ if [ "$key" = "G" ]; then
     git rm -r --cached .
     git add .
     git commit -m "[Git Fixed] .gitignore is now working"
-
     echo "Git Fixed"
 fi
