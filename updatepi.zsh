aa016648ce4b71bca5dc03d6ebc0c1175e1d11df
smittytone <smittytone@icloud.com>
2020-05-23 22:34:32 +0100

    Rename Pi update file; add zsh version

---
+++ updatepi.zsh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Update local user config files (eg. between multiple machines)
+# Version 2.0.0
+
+source="$HOME/Documents/GitHub/dotfiles"
+
+if ! [ -e $source ]; then
+    echo "Please clone the repo \'dotfiles\' before proceeding -- exiting "
+    exit 1
+fi
+
+echo "Updating primary config files... "
+
+# nano rc file
+cp -v "$source/nanorc" "$HOME/.nanorc"
+
+# bash profile
+cp -v "$source/pi_bash_aliases" "$HOME/.bash_aliases"
+
+# FROM 2.0.0
+# Sync .zshrc
+cp -v $source/pi_zshrc $HOME/.zshrc
+
+# gitup config
+if ! [ -e "$HOME/.config/gitup" ]; then
+    echo "Adding ~/.config/gitup... "
+    mkdir -p "$HOME/.config/gitup"
+fi
+
+cp -v "$source/pi_bookmarks" "$HOME/.config/gitup/bookmarks"
+
+if ! [ -e "$HOME/.config/git" ]; then
+    echo "Adding ~/.config/git... "
+    mkdir -p "$HOME/.config/git"
+fi
+
+# git global exclude file
+# Added it to partial install in 1.0.0
+if cp -v "$source/gitignore_global" "$HOME/.config/git/gitignore_global"; then
+    # Add a reference to the file to git (assumes git installed)
+    git config --global core.excludesfile "$HOME/.config/git/gitignore_global"
+fi
+
+echo "Configuration files updated"
