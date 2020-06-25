f7d51e297eee0290da15f1b62a7cee0d27cb9f27
Tony Smith <smittytone@icloud.com>
2020-06-25 14:26:27 +0100

    Make ~/bin if necessary

--- binstall.zsh
+++ binstall.zsh
@@ -8,9 +8,14 @@
 #      to list the files to be copied and made executable
 #
 # @version 1.0.0
-
+bin_dir=$HOME/bin
 source_file=$GIT/dotfiles/Mac/keyscripts
 scripts_dir=$GIT/scripts
+
+if [[ ! -e $bin_dir ]]; then
+    mkdir -p $bin_dir || echo 'Could not create ~/bin -- exiting' ; exit 1
+fi
+
 if [[ -e $source_file ]]; then
     if [[ -e $scripts_dir ]]; then
         # Read in each line of 'keyscripts', each of which
