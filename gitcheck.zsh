8f888d3b2c4da92d4825484cc2dfd6e47b249f70
Tony Smith <smittytone@icloud.com>
2020-12-14 14:47:00 +0000

    Add output for no changes

--- gitcheck.zsh
+++ gitcheck.zsh
@@ -7,12 +7,13 @@
 #
 # @author    Tony Smith
 # @copyright 2020, Tony Smith
-# @version   1.0.0
+# @version   1.0.1
 # @license   MIT
 #
 
 list=$(/bin/ls "$GIT")
 max=0
+output=0
 
 if cd "$GIT"; then
     # Get the longest file name
@@ -31,20 +32,25 @@ if cd "$GIT"; then
                 unmerged=$(git status)
                 unmerged=$(grep 'is ahead' < <((echo -e "$unmerged")))
                 if [[ -n "$unmerged" ]]; then
-                    state="unmerged"
+                    state="unmerged changes"
                 fi
 
                 uncommitted=$(git status --porcelain --ignore-submodules)
                 if [[ -n "$uncommitted" ]]; then
-                    state="uncommitted"
+                    state="uncommitted changes"
                 fi
 
                 if [[ -n "$state" ]]; then
                     awk '{ printf "%-*s %-50s\n", $1, $2, $3}' <((echo -e "$max $file $state"))
+                    ((output+=1))
                 fi
 
                 cd ..
             fi
         fi
     done
+fi
+
+if [[ $output -eq 0 ]]; then
+    echo "All repos up to date"
 fi
