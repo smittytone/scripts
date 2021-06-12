66e0faf62010c30d2c509e6f1e9acb22a6802ea2
smittytone <smittytone@icloud.com>
2021-06-12 14:52:11 +0100

    Better max length checking; remove awk

--- gitcheck.zsh
+++ gitcheck.zsh
@@ -6,12 +6,14 @@
 #
 # @author    Tony Smith
 # @copyright 2021, Tony Smith
-# @version   1.0.3
+# @version   1.1.0
 # @license   MIT
 
 
 local max=0
 local output=0
+local repos=()
+local states=()
 
 if [[ -z "$GIT" ]]; then
     echo 'Environment variable "$GIT" not set with your Git directory'
@@ -24,17 +26,10 @@ if [[ ! -d "$GIT" ]]; then
 fi
 
 if cd "$GIT"; then
-    # Get the longest file name
-    for file in *; do
-        if [[ ${#file} -gt $max ]]; then
-            max=${#file}
-        fi
-    done
-
     # Process the files
-    for file in *; do
-        if [[ -d "$file" && -d "$file/.git" ]]; then
-            if cd "$file"; then
+    for repo in *; do
+        if [[ -d "$repo" && -d "$repo/.git" ]]; then
+            if cd "$repo"; then
                 local state=""
                 local unmerged=$(git status)
                 unmerged=$(grep 'is ahead' < <((echo -e "$unmerged")))
@@ -48,8 +43,10 @@ if cd "$GIT"; then
                 fi
 
                 if [[ -n "$state" ]]; then
-                    awk '{ printf "%-*s %-50s\n", $1, $2, $3}' <((echo -e "$max $file $state"))
-                    ((output+=1))
+                    states+=("$state")
+                    repos+=("$repo")
+                    if [[ ${#repo} -gt $max ]] max=${#repo}
+                    output=1
                 fi
 
                 cd ..
@@ -60,6 +57,11 @@ fi
 
 if [[ $output -eq 0 ]]; then
     echo "All repos up to date"
+else
+    echo "Repos with changes:"
+    for (( i = 1 ; i <= ${#repos[@]} ; i++ )); do
+        printf '%*s has %s\n' $max ${repos[i]} ${states[i]}
+    done
 fi
 
 exit 0
