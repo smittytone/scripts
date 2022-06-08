1399b4354786e56d92e8da5d7f652fae19d07c49
smittytone <smittytone@icloud.com>
2022-06-08 06:06:10 +0100

    Update installer

--- pinstall.sh
+++ pinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Pi Installation Script 1.2.0
+# Pi Installation Script 1.3.0
 
 # Switch to home directory
 cd "$HOME" || exit 1
@@ -12,6 +12,7 @@ rm -rf Videos
 rm -rf python_games
 rm -rf Templates
 rm -rf MagPi
+rm -rf Bookshelf
 
 # Update the apt-get database, etc.
 echo "Updating system..."
@@ -22,29 +23,19 @@ sudo apt-get -y autoremove
 # Make directories
 echo "Creating directories..."
 mkdir "$HOME/GitHub"
-mkdir "$HOME/Python"
 
 # Update .bashrc
 echo "Configuring command line... "
 echo $'export PS1=\'$PWD > \'' >> .bashrc
 export PATH=$PATH:/usr/local/bin
-#echo "alias la='ls -lah --color=auto'" > .bash_aliases
-#echo "alias ls='ls -l --color=auto'" >> .bash_aliases
-#echo "alias rs='sudo shutdown -r now'" >> .bash_aliases
-#echo "alias sd='sudo shutdown -h now'" >> .bash_aliases
-#echo "alias update='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y'" >> .bash_aliases
 
 # Applications
 echo -n "Installing utilities: screen..."
 sudo apt-get -q -y install screen
-echo -n " nginx..."
-sudo apt-get -q -y install nginx
-echo -n " ruby..."
-sudo apt-get -q -y install ruby
 echo -n " scrot..."
 sudo apt-get -q -y install scrot
-echo -n " mdless..."
-sudo gem install -q --silent mdless
+echo -n " zsh..."
+sudo apt-get -q -y install zsh
 echo -n " pylint"
 sudo pip3 -q install pylint
 
@@ -65,16 +56,22 @@ if cd "$HOME/GitHub"; then
     git clone https://github.com/smittytone/scripts.git
 
     # Setup configs
-    cp dotfiles/pi_bash_aliases "$HOME"/.bash_aliases
-    cp dotfiles/nanorc "$HOME"/.nanorc
-    cp dotfiles/pylintrc "$HOME"/.pylintrc
-    cp dotfiles/gitignore_global "$HOME"/.gitignore_global
+    cp dotfiles/Pi/bash_aliases "$HOME"/.bash_aliases
+    cp dotfiles/Pi/nanorc "$HOME"/.nanorc
+    cp dotfiles/Universal/pylintrc "$HOME"/.pylintrc
+    cp dotfiles/Universal/gitignore_global "$HOME"/.gitignore_global
     git config --global core.excludesfile "$HOME"/.gitignore_global
 
     # From 1.1.0
     # Setup and enable VNC service
-    sudo cp dotfiles/pi_virtual_desktop.service /etc/systemd/system/vnc_vd.service
+    sudo cp dotfiles/Pi/pi_virtual_desktop.service /etc/systemd/system/vnc_vd.service
     sudo systemctl enable vnc_vd.service
+fi
+
+# FROM 1.3.0 set Z as default shell
+z_path=$(which zsh)
+if [[ -f "${z_path}" ]]; then
+    chsh -s "${z_path}"
 fi
 
 # Remove the script
