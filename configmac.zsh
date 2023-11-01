9c055bd174c3dfa067d8ab8efcf61000905289de
smittytone <smittytone@icloud.com>
2023-11-01 15:36:00 +0000

    Update Mac scripts

--- configmac.zsh
+++ configmac.zsh
@@ -11,20 +11,20 @@ file_target="$HOME/Library"
 echo "Updating primary config files... "
 
 # bash profile
-cp -v "$file_source/Mac/bash_profile" "$HOME/.bash_profile"
+cp -v "${file_source}/Mac/bash_profile" "$HOME/.bash_profile"
 
 # ZSH rc file
-cp -v "$file_source/Mac/zshrc_base" "$HOME/.zshrc"
+cp -v "${file_source}/Mac/zshrc_base" "$HOME/.zshrc"
 echo "export PICO_SDK_PATH=$HOME/GitHub/pico-sdk" > "$HOME/.exports"
 
 # nano rc file
-cp -v "$file_source/Mac/nanorc" "$HOME/.nanorc"
+cp -v "${file_source}/Mac/nanorc" "$HOME/.nanorc"
 
 # vscode settings
-cp -v "$file_source/Mac/vs_settings.json" "$file_target/Application Support/Code/User/settings.json"
+cp -v "${file_source}/Mac/vs_settings.json" "${file_target}/Application Support/Code/User/settings.json"
 
 # pylint rc file
-cp -v "$file_source/Universal/pylintrc" "$HOME/.pylintrc"
+cp -v "${file_source}/Universal/pylintrc" "$HOME/.pylintrc"
 
 # git config
 if [[ ! -e "$HOME/.config/git" ]]; then
@@ -34,25 +34,25 @@ fi
 
 # git global exclude file
 # Added it to partial install in 1.0.3
-if cp -v "$file_source/Universal/gitignore_global" "$HOME/.config/git/gitignore_global"; then
+if cp -v "${file_source}/Universal/gitignore_global" "$HOME/.config/git/gitignore_global"; then
     # Add a reference to the file to git (assumes git installed)
     git config --global core.excludesfile "$HOME/.config/git/gitignore_global"
 fi
 
 echo "Updating additional config files... "
-cp -nvR "$file_source/Mac/Services/" "$file_target/Services"
+cp -nvR "${file_source}/Mac/Services/" "${file_target}/Services"
 
-cp -nv "$file_source/Mac/HomebrewMe.terminal" "$HOME/Desktop/HomebrewMe.terminal"
-cp -nv "$file_source/Mac/HomebrewMeDark.terminal" "$HOME/Desktop/HomebrewMeDark.terminal"
+cp -nv "${file_source}/Mac/HomebrewMe.terminal" "$HOME/Desktop/HomebrewMe.terminal"
+cp -nv "${file_source}/Mac/HomebrewMeDark.terminal" "$HOME/Desktop/HomebrewMeDark.terminal"
 echo "Terminal settings files 'HomebrewMe' and 'HomebrewMeDark' copied to desktop. To use them, open Terminal > Preferences > Profiles and import"
 
 # Run the various macOS config scriptlets
 echo "Configuring macOS... "
-if cd "$file_source/Mac/config"; then
+if cd "${file_source}/Mac/config"; then
     chmod +x ./*
     for task in *; do
-        echo $task
-        ./$task
+        echo ${task}
+        source ${task}
     done
 fi
 
--- setupmac.zsh
+++ setupmac.zsh
@@ -7,7 +7,7 @@
 #
 # @author    Tony Smith
 # @copyright 2020, Tony Smith
-# @version   3.0.0
+# @version   3.0.1
 # @license   MIT
 #
 
@@ -20,14 +20,14 @@ function show_errors() {
             echo "  ${count}. ${error}"
             (( count+=1 ))
         done
+        exit 1
     fi
-    exit 1
 }
 
 
 APP_NAME=$(basename $0)
 APP_NAME=${APP_NAME:t}
-APP_VERSION="3.0.0"
+APP_VERSION="3.0.1"
 errors=()
 
 # Do intro
@@ -97,21 +97,21 @@ if /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install
     echo "Installing Brew-sourced Utilities... "
     apps=(bash nano coreutils gitup jq ncurses readline shellcheck libdvdcss node python3 hugo "sass/sass/sass" sphinx-doc)
     for app in "${apps[@]}"; do
-        brew install "$app"
+        brew install "${app}"
     done
 
     echo "Installing Applications... "
-    apps=("handbrake" "vlc" "skype" "firefox" "omnidisksweeper")
+    apps=(handbrake skype firefox omnidisksweeper)
     for app in "${apps[@]}"; do
-        brew install --cask "$app"
+        brew install --cask "${app}"
     done
 
     # FROM 2.2.0
     echo "Installing My Applications... "
     brew tap smittytone/homebrew-smittytone
-    apps=(mnu pdfmaker imageprep ascii the-valley utit)
+    apps=(ascii mnu imageprep pdfmaker squinter the-valley utitool)
     for app in "${apps[@]}"; do
-        brew install --cask "$app"
+        brew install --cask "${app}"
     done
 
     # FROM 3.0.0
@@ -119,15 +119,15 @@ if /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install
 else
     echo "Could not install Brew"
     errors+="Brew installation"
-    show_errors()
+    show_errors
 fi
 
 # Set up git and clone key repos
 echo "Preparing Git..."
 target="$HOME/GitHub"
-[[ ! -e "$target" ]] && mkdir "$target"
+[[ ! -e "${target}" ]] && mkdir "${target}"
 
-cd "$target" || show_errors()
+cd "${target}" || show_errors
 [[ ! -e scripts ]] && git clone https://github.com/smittytone/scripts.git
 [[ ! -e dotfiles ]] && git clone https://github.com/smittytone/dotfiles.git
 
@@ -139,8 +139,8 @@ scripts/configmac.zsh
 #echo "Installing Cocoapods (requires authorizaton)... "
 #sudo gem install cocoapods
 
-echo "Installing Python modules... "
-pip3 install pylint sphinx-rtd-theme
+#echo "Installing Python modules... "
+#pip3 install pylint sphinx-rtd-theme
 
 # FROM 2.1.0
 # Install node packages
@@ -151,36 +151,6 @@ if [[ $? -eq  0 ]]; then
     npm install -g uglifycss
 fi
 
-# Offer to open the App Store
-read -k -s "key?Press [ENTER] to open the App Store, or [S] to skip"
-echo
-key=${key:l}
-[[ "$key" != "s" ]] && open '/Applications/App Store.app'
-
-# Offer to open other app websites
-read -k -s "key?Press [ENTER] to open websites for other app downloads, or [S] to skip "
-echo
-key=${key:l}
-if [[ "$key" != "s" ]]; then
-    open http://www.rogueamoeba.com/piezo
-    open https://www.bresink.com/osx/TinkerTool/download.php
-    open http://www.audacityteam.org/download/mac/
-    open https://www.sketch.com
-    #open http://www.skype.com
-    #open http://handbrake.fr
-    #open http://www.mozilla.org
-    #open http://www.skype.com
-    #open http://www.videolan.org
-    #open http://www.dropbox.com
-    #open https://desktop.github.com
-fi
-
-# Offer to open a backup drive
-read -k -s "key?Connect drive '500GB' and press [ENTER] to copy music, or [S] to skip "
-echo
-key=${key:l}
-[[ "$key" != "s" ]] && cp -R /Volumes/500GB/Music/ "$HOME/Music"
-
 # All done
 read -k -s "key?Press [ENTER] to finish "
 echo
@@ -190,7 +160,7 @@ brew cleanup
 
 # FROM 2.2.0
 # Report any issues encountered
-show_errors()
+show_errors
 
 echo "Done"
 exit 0
