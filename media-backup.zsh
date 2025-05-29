da36ae29e9cd07695700db0a19c774a711221c87
Tony Smith (smittytone) <tony@smittytone.net>
2025-05-29 18:48:20 +0100

    Improve output

--- media-backup.zsh
+++ media-backup.zsh
@@ -6,8 +6,8 @@
 # Backup to Disk Script
 #
 # @author    Tony Smith
-# @copyright 2024, Tony Smith
-# @version   1.0.0
+# @copyright 2025, Tony Smith
+# @version   1.0.1
 # @license   MIT
 #
 
@@ -20,13 +20,16 @@ music_sources=(Alternative Classical Comedy 'Doctor Who' Electronic Folk Pop Met
                SFX Singles Soundtracks 'Spoken Word' Instrumental)
 home_sources=(Comics)
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 # Functions
 do_sync() {
     # Sync the source to the target
     # Arg 1 should be the source directory
     # Arg 2 should be the target directory
     local name="${1:t}"
-    echo -n "Syncing ${name}... "
+    echo -n "  Syncing ${name}... "
 
     # Prepare a readout of changed files ONLY (rsync does not do this)
     local list=$(rsync -az "$1" "$2" -i --exclude ".DS_Store")
@@ -42,10 +45,13 @@ do_sync() {
         done <<< "${lines}"
         echo "${count} files changed:"
         # Output the files changed
+        count=1
         while IFS= read -r line; do
             # rsync 2.6.x use 'cut -c 11-'; below is for 3.3.x
-            local trimline=$(echo "${line}" | cut -c 14-)
-            [[ -n "${trimline}" ]] && echo "    /${trimline}"
+            local trimmed=$(echo "${line}" | cut -c 14-)
+            number=$(printf "%03d" ${count})
+            [[ -n "${trimmed}" ]] && echo "    ${number}. /${trimmed}"
+            ((count++))
         done <<< "$lines"
     else
         echo "no files changed"
@@ -90,7 +96,7 @@ for arg in "$@"; do
     fi
 done
 
-# Check that the user is not exluding both jobs
+# Check that the user is not excluding both jobs
 [[ ${do_books} -eq 0 && ${do_music} -eq 0 ]] && show_error_and_exit "Mutually exclusive options set -- backup cannot continue"
 
 # Set the target path based on supplied disk name (or default)
@@ -98,38 +104,38 @@ target_path="/Volumes/${target_disk}"
 
 # Make sure the target disk is mounted
 if [[ -d "${target_path}" ]]; then
-    echo "Disk '${target_disk}' mounted."
+    echo "Disk ${bold}${target_disk}${normal} mounted."
 
     # Sync document sources
     if [[ ${do_books} -eq 1 ]]; then
-        echo "Syncing locally hosted items"
+        echo "${bold}Locally hosted items${normal}"
         for source in "${local_sources[@]}"; do
             do_sync "${HOME}/${source}" "${target_path}"
         done
 
-        echo "Syncing server-hosted items"
+        echo "${bold}Server-hosted items${normal}"
         if [[ -d /Volumes/home ]]; then
             for source in "${home_sources[@]}"; do
                 do_sync "/Volumes/home/${source}" "${target_path}"
             done
         else
-            show_error_and_exit "'Home' server not mounted"
+            show_error_and_exit "${bold}Home${normal} server not mounted"
         fi
     fi
 
     # Sync music sources
     if [[ ${do_music} -eq 1 ]]; then
-        echo "Syncing server-hosted music"
+        echo "${bold}Server-hosted music${normal}"
         if [[ -d /Volumes/Music ]]; then
             for source in "${music_sources[@]}"; do
                 do_sync "/Volumes/Music/${source}" "${target_path}/Music"
             done
         else
-            show_error_and_exit "'Music' server not mounted"
+            show_error_and_exit "${bold}Music${normal} server not mounted"
         fi
     fi
 else
-    show_error_and_exit "Disk '${target_disk}' is not mounted."
+    show_error_and_exit "Disk ${bold}${target_disk}${normal} is not mounted."
 fi
 
 exit 0
